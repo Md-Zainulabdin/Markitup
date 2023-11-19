a3ba3bc8970b7887778fe7096c86b10ad68e08dc
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2023-11-19 23:17:29 +0500

    Add favicon & title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Markitup",
+  title: "MarkitUp - Content Marketing at its best",
   description: "A Content Marketing Agency",
 };
 
