f695217ca2cdb7d6fc3ada5aa3c759f076a7d3a9
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2023-11-20 15:31:05 +0500

    Build user post API

---
+++ src/app/(api-routes)/api/user/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+import { hash } from "bcrypt";
+import prismadb from "@/lib/prisma";
+
+export const POST = async (request: NextRequest) => {
+  const { name, email, password, role, avatar } = await request.json();
+
+  if (!name || !email || !password || !role || avatar) {
+    return new NextResponse("All feilds are required!", { status: 500 });
+  }
+
+  const hashedPassword = await hash(password, 10);
+
+  try {
+    const admin = await prismadb.admin.create({
+      data: {
+        name,
+        email,
+        role,
+        avatar,
+        password: hashedPassword,
+      },
+    });
+
+    return NextResponse.json(admin, { status: 201 });
+  } catch (error) {
+    console.log("USER-POST", error);
+    return new NextResponse("Internal Error", { status: 400 });
+  }
+};
