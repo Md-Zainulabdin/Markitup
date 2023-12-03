f71045a8be49f237cf27d30671bf8973b73b8bfc
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2023-12-03 23:17:48 +0500

    add services

--- src/app/(main)/(root)/components/Services.tsx
+++ src/app/(main)/(root)/components/Services.tsx
@@ -2,15 +2,50 @@ import { TfiWorld } from "react-icons/tfi";
 
 const serviceCards = [
   {
-    icon: (
-      <div className="my-2 w-[60px] h-[60px] flex items-center justify-center rounded-full bg-[#e1eeff]">
-        <TfiWorld size={22} className="text-[#2282ff]" />
-      </div>
-    ),
     title: "Social Media Management",
     subHeading: "Instagram, Linkedin and more",
     description: `Amplify your social media impact through expert management services. We devise captivating
     strategies for heightened engagement, follower growth, and extensive reach.`,
+  },
+
+  {
+    title: "360° Youtube Management",
+    subHeading: "For Brands and Influencers",
+    description: `All-encompassing support for startegy, post-production, operations, and analytics, driving
+    channel growth and success."
+  `,
+  },
+  {
+    title: "Graphic Designing",
+    subHeading: "Mutipurpose Designing ",
+    description: `Elevate your brand with versatile, tailored multipurpose graphic design services: stunning
+    visuals for marketing, branding, and social media.
+  "
+  `,
+  },
+  {
+    title: "Video Editing",
+    subHeading: "Vlogs, Tutorials, Educational etc.",
+    description: `Transform your raw footage into stunning, engaging videos with our professional video
+    editing services. From concept to delivery, we bring your story to life. 
+  "
+  `,
+  },
+  {
+    title: " Advertising",
+    subHeading: "Facebook, Instagram and Google",
+    description: `Empower your brand with Facebook, Instagram, Google, and YouTube advertising. Reach
+    millions, boost sales, and maximize visibility.  
+  "
+  `,
+  },
+  {
+    title: " Social Media Consulting",
+    subHeading: "Calls for building strategy",
+    description: ` Maximize YouTube success with expert consulting: in-depth channel analysis, innovative
+    video ideas, editing/thumbnail feedback, and content optimization.   
+  "
+  `,
   },
 ];
 
@@ -23,24 +58,31 @@ const Services: React.FC = () => {
         </h1>
       </div>
 
-      <div className="services-carda w-full flex items-center justify-center flex-wrap gap-8 py-8">
+      <div className="services-cards w-full flex items-center justify-center flex-wrap gap-8 py-10">
         {serviceCards.map((card) => (
           <div
-            className="w-[350px] p-6 bg-[#fff] flex flex-col items-start gap-3 border border-[#f8f8f8] hover:border-[#ccc] rounded-md duration-300 ease-in-out cursor-pointer"
+            className="w-[350px] p-8 bg-[#fff] flex flex-col items-start gap-3 border border-[#f8f8f8] hover:border-[#ccc] rounded-md duration-300 ease-in-out cursor-pointer"
             key={card.title}
           >
             {/* <div className="icon">{card.icon}</div> */}
 
             <div className="title">
-              <h1 className="text-xl font-bold inter-font text-[#222]">{card.title}</h1>
+              <h1 className="text-xl font-bold inter-font text-[#222]">
+                {card.title}
+              </h1>
             </div>
 
-            <div className="sub-heading">
-                <h2>{card.subHeading}</h2>
+            <div className="sub-heading font-semibold text-[#555]">
+              <h2>{card.subHeading}</h2>
             </div>
 
             <div>
-                <p className="text-sm text-muted-foreground" id="services_paragraph">{card.description}</p>
+              <p
+                className="text-sm text-muted-foreground"
+                id="services_paragraph"
+              >
+                {card.description}
+              </p>
             </div>
           </div>
         ))}
