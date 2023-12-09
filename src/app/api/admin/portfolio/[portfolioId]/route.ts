355b9aefea825d3fe725d663ab4bdc087a474b83
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2023-12-09 14:59:47 +0500

    portfolio API update

--- src/app/api/admin/portfolio/[portfolioId]/route.ts
+++ src/app/api/admin/portfolio/[portfolioId]/route.ts
@@ -1,9 +1,16 @@
 import prismadb from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
+// Define a type for the expected params structure
+type Params = {
+    params: {
+        portfolioId: string;
+    };
+  };
+
 export const PATCH = async (
   request: NextRequest,
-  { params }: { params: { portfolioId: string } }
+  { params }: Params
 ) => {
   try {
     const { title, description, imageUrl, category, projectLink } =
@@ -41,3 +48,25 @@ export const PATCH = async (
     return new NextResponse("Internal Error", { status: 400 });
   }
 };
+
+
+export const DELETE = async (
+    request: NextRequest,
+    { params }: Params
+  ) => {
+    try {
+
+      if (!params.portfolioId) {
+        return new NextResponse("Portfolio id is required!", { status: 400 });
+      }
+  
+      const deletedPortfolio = await prismadb.portfolio.delete({
+        where: { id: params.portfolioId },
+      });
+  
+      return NextResponse.json(deletedPortfolio, { status: 201 });
+    } catch (error) {
+      console.log("PORTFOLIO-DELETE", error);
+      return new NextResponse("Internal Error", { status: 400 });
+    }
+  };
--- src/app/api/admin/service/[serviceId]/route.ts
+++ src/app/api/admin/service/[serviceId]/route.ts
@@ -1,20 +1,33 @@
 import { NextRequest, NextResponse } from "next/server";
 import prismadb from "@/lib/prisma";
 
+// Define a type for the expected params structure
+type Params = {
+  params: {
+    serviceId: string;
+  };
+};
+
+// PATCH endpoint for updating a service
 export const PATCH = async (
   request: NextRequest,
-  { params }: { params: { serviceId: string } }
+  { params }: Params 
 ) => {
+  // Extract data from the request body
   const { heading, subHeading, description } = await request.json();
 
+  // Check if required fields are present in the request body
   if (!heading || !subHeading || !description) {
-    return new NextResponse("All feilds are required!", { status: 500 });
+    return new NextResponse("All fields are required!", { status: 500 });
   }
 
+  // Check if the serviceId is present in the params
   if (!params.serviceId) {
     return new NextResponse("Service Id is required!", { status: 400 });
   }
+
   try {
+    // Update the service in the database
     const updatedService = await prismadb.service.update({
       where: {
         id: params.serviceId,
@@ -25,32 +38,39 @@ export const PATCH = async (
         description,
       },
     });
+
+    // Return the updated service as JSON response
     return NextResponse.json(updatedService, { status: 201 });
   } catch (error) {
+    // Handle errors and log them
     console.log("SERVICE-PATCH", error);
     return new NextResponse("Internal Error", { status: 400 });
   }
 };
 
-
+// DELETE endpoint for deleting a service
 export const DELETE = async (
-    request: NextRequest,
-    { params }: { params: { serviceId: string } }
-  ) => {
-  
-    if (!params.serviceId) {
-      return new NextResponse("Service Id is required!", { status: 400 });
-    }
-    try {
-      const deletedService = await prismadb.service.delete({
-        where: {
-          id: params.serviceId,
-        }
-      });
-      return NextResponse.json(deletedService, { status: 201 });
-    } catch (error) {
-      console.log("SERVICE-DELETE", error);
-      return new NextResponse("Internal Error", { status: 400 });
-    }
-  };
-  
+  request: NextRequest,
+  { params }: Params
+) => {
+  // Check if the serviceId is present in the params
+  if (!params.serviceId) {
+    return new NextResponse("Service Id is required!", { status: 400 });
+  }
+
+  try {
+    // Delete the service from the database
+    const deletedService = await prismadb.service.delete({
+      where: {
+        id: params.serviceId,
+      }
+    });
+
+    // Return the deleted service as JSON response
+    return NextResponse.json(deletedService, { status: 201 });
+  } catch (error) {
+    // Handle errors and log them
+    console.log("SERVICE-DELETE", error);
+    return new NextResponse("Internal Error", { status: 400 });
+  }
+};
